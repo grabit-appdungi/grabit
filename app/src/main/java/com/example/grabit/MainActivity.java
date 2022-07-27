f05e914705b446ce32f1a068b73e5b1b088683f9
river99 <qkrrkd42@naver.com>
2022-07-28 00:47:16 +0900

    main

--- app/src/main/java/com/example/grabit/MainActivity.java
+++ app/src/main/java/com/example/grabit/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.grabit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.app.DatePickerDialog;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.grabit.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     TextView dateText;
+    int progress;
+
     DatePickerDialog datePickerDialog;
+    private ArrayList<MainData> arrayList;
+    private MainAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,43 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        recyclerView = binding.rcviewMain;
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+        arrayList.add(new MainData("물 마시기","40"));
+        arrayList.add(new MainData("걷기","80"));
+        arrayList.add(new MainData("아침밥 먹기","100"));
+        arrayList.add(new MainData("독서하기","45"));
+        arrayList.add(new MainData("아침에 일어나기","0"));
+        arrayList.add(new MainData("스트레칭 하기","50"));
+        arrayList.add(new MainData("영어공부 하기","20"));
+        arrayList.add(new MainData("물 마시기","40"));
+        arrayList.add(new MainData("걷기","80"));
+        arrayList.add(new MainData("아침밥 먹기","100"));
+        arrayList.add(new MainData("독서하기","45"));
+        arrayList.add(new MainData("아침에 일어나기","0"));
+        arrayList.add(new MainData("스트레칭 하기","50"));
+        arrayList.add(new MainData("영어공부 하기","20"));
+
+        mainAdapter = new MainAdapter(arrayList);
+        recyclerView.setAdapter(mainAdapter);
+
         dateText = binding.txtDate;
+
+        if(arrayList != null){
+            int i = 0;
+            progress = 0;
+            while(i < arrayList.size()){
+                progress = progress + Integer.parseInt(arrayList.get(i).getHabit_num().toString());
+                i++;
+            }
+            progress = progress / arrayList.size() ;
+        }
+        ProgressBar progressBar = binding.pgbarMain;
+        progressBar.setProgress(progress);
+        binding.txtProgressNumber.setText(String.valueOf(progress));
 
         dateText.setOnClickListener(new View.OnClickListener() {
             @Override
